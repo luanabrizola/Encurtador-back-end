1bde41d7f1965a9e9e0d1a3fd583598763f93a93
Ana Julia Menegasso <130059630+AnaMenegasso@users.noreply.github.com>
2025-10-08 11:09:22 -0300

    classe LinkService com métodos CRUD
    implementando classe LinkService para centralizar regras de negócio e operações CRUD com o repositório

--- src/modules/links/link.service.js
+++ src/modules/links/link.service.js
@@ -1,1 +1,28 @@
-"."
+export class LinkService {
+
+    constructor(linkRepository) {
+        this.linkRepository = linkRepository;
+    }
+
+    getAllLinks() {
+        return this.linkRepository.findAll();
+    }
+
+    getLinkById(id) {
+        return this.linkRepository.findById(id);
+    }
+
+    createLink(linkData) {
+        // No futuro, regras de negócio como "verificar email duplicado"
+        // viveriam aqui, ANTES de chamar o repositório.
+        return this.linkRepository.create(linkData);
+    }
+
+    updateLink(id, linkData) {
+        return this.linkRepository.update(id, linkData);
+    }
+
+    deleteLink(id) {
+        return this.linkRepository.remove(id);
+    }
+}
