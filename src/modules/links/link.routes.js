3a04d4f2acec6fb9cacc178ae537e14f37eaf510
Ana Julia Menegasso <130059630+AnaMenegasso@users.noreply.github.com>
2025-10-08 11:17:06 -0300

    Criando as rotas

--- src/modules/links/link.routes.js
+++ src/modules/links/link.routes.js
@@ -1,1 +1,26 @@
-"."
+import { LinkRepository } from './link.repository.js';
+import { LinkService } from './link.service.js';
+
+const linkRepository = new LinkRepository();
+const linkService = new LinkService(linkRepository);
+
+
+// export async function linkRoutes(fastify, options) {
+
+
+//     fastify.get('/links', (request, reply) =>
+//         linkController.getLinks(request, reply)
+//     );
+//     fastify.get('/links/:id', (request, reply) =>
+//         linkController.getLinkById(request, reply)
+//     );
+//     fastify.post('/links', (request, reply) =>
+//         linkController.createLink(request, reply)
+//     );
+//     fastify.put('/links/:id', (request, reply) =>
+//         linkController.updateLink(request, reply)
+//     );
+//     fastify.delete('/links/:id', (request, reply) =>
+//         linkController.deleteLink(request, reply)
+//     );
+// }
