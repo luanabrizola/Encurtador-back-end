ec59f09ccbdb68f9c6aecc933230cc37910a7d82
luanabrizola <130062674+luanabrizola@users.noreply.github.com>
2025-10-22 09:29:21 -0300

    Adicionando visualizações

--- src/infra/db/schema.js
+++ src/infra/db/schema.js
@@ -1,4 +1,4 @@
-import { pgTable, uuid, text, varchar } from 'drizzle-orm/pg-core';
+import { pgTable, uuid, text, varchar, integer } from 'drizzle-orm/pg-core';
 
 // Definimos a tabela "links", espelhando a estrutura do nosso SQL
 export const links = pgTable('links', {
@@ -6,4 +6,5 @@ export const links = pgTable('links', {
     legenda: text('legenda').notNull(),
     url: text('url').notNull().unique(),
     codigo: varchar('codigo', { length: 10 }).notNull().unique(),
+    visualizacoes: integer('visualizacoes').default(0),
 });
